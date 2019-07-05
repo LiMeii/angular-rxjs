8a3dc6b1f2b5f80c24e4c5abae7dc27e8d7f53ba
mei li <mei90s.lee@gmail.com>
2019-07-05 22:09:14 +0800

    update

--- src/app/modules/rxjs-learning/rxjs-learning.component.ts
+++ src/app/modules/rxjs-learning/rxjs-learning.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
-import { fromEvent } from "rxjs";
-import { map, scan } from 'rxjs/operators';
+import { fromEvent, Subscription } from "rxjs";
+import { map, scan, buffer, throttleTime, filter } from 'rxjs/operators';
+
 
 @Component({
     templateUrl: "./rxjs-learning.component.html"
@@ -8,18 +9,43 @@ import { map, scan } from 'rxjs/operators';
 
 export class RxjsLearningComponent implements OnInit {
 
+    subscription: Subscription;
+
     ngOnInit() {
-        const button = document.getElementById("myBtn");
+         this.getClickCounter();
+        //this.getDouleClickCounter();
 
-        const clickStream = fromEvent(button, "click");
+    }
 
-        const counterStream = clickStream.pipe(
+    getClickCounter() {
+        let button = document.getElementById("myBtn");
+
+        let clickStream$ = fromEvent(button, "click");
+
+        let counterStream$ = clickStream$.pipe(
             map((data) => { return 1 }),
             scan((acc, curr) => acc + curr, 0)
         );
 
-        counterStream.subscribe(data => {
-            console.log('this is the click counter: ' + data);
+        counterStream$.subscribe(data => {
+            console.log("this is the click counter: " + data);
+        });
+    }
+
+    getDouleClickCounter() {
+        let button = document.getElementById("myBtn");
+
+        let clickStream$ = fromEvent(button, "click");
+
+        let doubleClickStream$ = clickStream$
+            .pipe(
+                buffer(clickStream$.pipe(throttleTime(250))),
+                map(click => { return click.length }),
+                filter(num => num >= 2)
+            )
+
+        doubleClickStream$.subscribe(data => {
+            console.log("the number of double click is: " + data);;
         });
     }
 }
