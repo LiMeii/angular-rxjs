f8f777ff2569100ef6777e1ce5834817dd172fc9
mei li <mei90s.lee@gmail.com>
2019-08-14 23:08:56 +0800

    show hide notification

--- src/app/modules/rxjs-notification/rxjs-notification.component.ts
+++ src/app/modules/rxjs-notification/rxjs-notification.component.ts
@@ -4,7 +4,7 @@ import { Observable, Subject, merge } from "rxjs";
 import { User } from "./interface/rxjs-notification.interface";
 
 import { RxjsNotificationService } from "./service/rxjs-notification.service";
-import { take, mergeMap } from 'rxjs/operators';
+import { take, mergeMap, skip, mapTo } from 'rxjs/operators';
 
 @Component({
     templateUrl: "./rxjs-notification.component.html"
@@ -13,7 +13,8 @@ import { take, mergeMap } from 'rxjs/operators';
 
 export class RxjsNotificationComponent {
     private users$: Observable<Array<User>>;
-    updateClick$ = new Subject<void>();
+    //updateClick$ = new Subject<void>();
+    showNotificatoin$: Observable<boolean>;
     forceReload$ = new Subject<void>()
 
     constructor(private rxjsNotificationService: RxjsNotificationService) { }
@@ -21,9 +22,18 @@ export class RxjsNotificationComponent {
     ngOnInit() {
         const initialUsers$ = this.getUserOnce();
 
-        const updateUsers$ = merge(this.updateClick$, this.forceReload$).pipe(
+        // const updateUsers$ = merge(this.updateClick$, this.forceReload$).pipe(
+        //     mergeMap(() => this.getUserOnce())
+        // )
+
+        const updateUsers$ = this.forceReload$.pipe(
             mergeMap(() => this.getUserOnce())
         )
+
+        const initNotification$ = this.rxjsNotificationService.users.pipe(skip(1));
+        const show$ = initNotification$.pipe(mapTo(true));
+        const hide$ = updateUsers$.pipe(mapTo(false));
+        this.showNotificatoin$ = merge(show$, hide$);
 
         this.users$ = merge(initialUsers$, updateUsers$);
 
